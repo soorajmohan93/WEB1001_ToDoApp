07edc4951232a471504738bd7869be6c0b77450e
soorajmohan93 <90652975+soorajmohan93@users.noreply.github.com>
2021-11-19 23:41:34 -0500

    Initial Commit
    Models and To Do Page

---
+++ ToDoApplication/Models/ToDos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApplication.Models
+{
+    public class ToDos
+    {
+        [StringLength(256, MinimumLength = 5)]
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        [Timestamp]
+        public DateTime CompletedDate { get; set; }
+    }
+}
---
+++ ToDoApplication/Pages/ToDoPage.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoApplication.Pages
+{
+    public class ToDoPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
